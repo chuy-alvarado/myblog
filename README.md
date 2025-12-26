9ad6b68cbe3b20ce6784c7c89fa6045bec59f125
chuy-alvarado <chuy.alvaradogarzon@wgu.edu>
2025-12-26 17:56:40 -0600

    feat: create fixtures

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# My personal blog for WGU career 
+# My personal blog for WGU career
 
 ## Selecting career path
 
@@ -28,4 +28,11 @@
 `$ uv sync`
 
 
+### Create a superuser first (if not exists)
+`uv run python manage.py createsuperuser`
 
+### Load the fixtures
+`uv run python manage.py loaddata posts`
+
+### Or create fixtures from existing data
+`uv run python manage.py dumpdata blog.post --indent 2 > blog/fixtures/posts.json`
--- blog/views.py
+++ blog/views.py
@@ -9,19 +9,19 @@ class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
     permission_classes = [AllowAny]
-    
+
     @action(detail=False, methods=['get'], url_path='myposts')
     def myposts(self, request):
         posts = Post.objects.all()
         serializer = self.get_serializer(posts, many=True)
         return Response(serializer.data)
-    
+
     @action(detail=False, methods=['get'], url_path='latest')
     def latest(self, request):
         posts = Post.objects.order_by('-publish_date')[:5]
         serializer = self.get_serializer(posts, many=True)
         return Response(serializer.data)
-    
+
     @action(detail=False, methods=['get'], url_path='featured')
     def featured(self, request):
         posts = Post.objects.filter(featured=True)
